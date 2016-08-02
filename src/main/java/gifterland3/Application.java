1426fe71ddf10aa7236ce422d07b7d0f47bace76
Ygarr <Urbanterror1>
2016-08-02 19:39:32 +0300

    pages thymeleaf

--- src/main/java/gifterland3/Application.java
+++ src/main/java/gifterland3/Application.java
@@ -2,9 +2,6 @@ package gifterland3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @SpringBootApplication
 public class Application {
@@ -21,15 +18,5 @@ public class Application {
 //        return Server.createWebServer("-web","-webAllowOthers","-webDaemon","-webPort", "8082");
 //    }
 
-
-    //from ~/src/main/webapp/WEB-INF/applicationContext.xml
-    @Bean
-    public UrlBasedViewResolver setupViewResolver() {
-        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix("");//empty not .jsp to serve either index.html or index.jsp
-        resolver.setViewClass(JstlView.class);
-        return resolver;
-    }
 
 }
--- src/main/java/gifterland3/controllers/MainController.java
+++ src/main/java/gifterland3/controllers/MainController.java
@@ -1,17 +1,77 @@
 package gifterland3.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
 
 @Controller
 public class MainController {
 
-  @RequestMapping("/")
-  @ResponseBody
-  public String index() {
-    return "Proudly handcrafted by " +
-        "<a href='http://localhost:8080/'>gifterland3</a> :)";
-  }
+
+//    @RequestMapping(value = {"/index", "/home", "/", }, method = RequestMethod.GET)
+//    public String homePage() {
+//
+//        return "home";
+//    }
+
+//  @RequestMapping(value = {"/gifterland3","/index","/home","/main.html"},method = RequestMethod.GET)
+//  @ResponseBody
+//  public ModelAndView mainIndex() {  //was String ModelAndView
+//      String message = "Proudly handcrafted by " +
+//              "<a href='http://localhost:8080/'>localhost</a> :B";
+// //     return  message;
+//
+////    return "Proudly handcrafted by " +
+////        "<a href='http://localhost:8080/'>gifterland3</a> :)";
+//      return new ModelAndView(message);
+//  }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Here must be your login, " +
+                "<a href='http://localhost:8080/'>someone</a> !!!";
+    }
+
+
+    @RequestMapping("/something")
+    @ResponseBody
+    public String helloWorld() {
+        String now = (new Date()).toString();
+        return "Sup! You! it is: " + now;
+    }
+
+    @RequestMapping("/reset")
+    public String Reset() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/main")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String messagHTML = "ModelAndView by " +
+                "<a href='http://localhost:8080/'>localhost</a> :B";
+
+        String messagThymeLeaf = "ModelAndView by  localhost :-B";
+
+        //return new ModelAndView("/WEB-INF/views/main.html", "message", messaga);
+        return new ModelAndView("main", "message", messagThymeLeaf);
+    }
+
+   //http://localhost:8080/greeting?name=User.
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 
 }
