5758446c43b496a0fa3eff4fbf8cdd97dc34d8b2
Ygarr <Urbanterror1>
2016-08-17 23:42:20 +0300

    Meal - Product entity

--- src/main/java/gifterland3/models/Meal.java
+++ src/main/java/gifterland3/models/Meal.java
@@ -1,10 +1,66 @@
 package gifterland3.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
 /**
- * Created by kdiv on 8/1/16.
- *  картинка,название, цена, страна производитель, штрихкод товара
+ * Created by kdiv on 8/1/16.  meal or Product
+ * картинка,название, цена, страна производитель, штрихкод товара
  */
+@Entity
+@Table(name = "meal")
 public class Meal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+
+    @NotNull
+    private String name;
+    private BigDecimal price;
+    private String manufacturer;//TODO справочник
+    private Integer barcode;
+
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private ProductCategory category;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
 
 
 }
