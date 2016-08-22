52e350a5f025be92bbaeb1b7919aae190627ee32
Ygarr <Urbanterror1>
2016-08-22 03:45:14 +0300

    Removed default DB. Used specified DB. Used SQL scripts to insert (populate) DATA

---
+++ src/main/resources/sql/h2db/data-h2.sql
@@ -0,0 +1,2 @@
+--data.sql
+INSERT INTO foodstores VALUES (1, 'Super Grocery Store', 'whatsyour@ml.eu', 'Поставляем свежакъ', 'Madison', 'Deputatov','Deputatovich','Конча Заспа, 23-567','608-555-1023');
