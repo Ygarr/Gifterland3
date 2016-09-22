3afe4173e53d8996d6714dc3e8b19dc6ad54dbb4
Ygarr <Urbanterror1>
2016-09-22 09:26:01 +0300

    facebook friend list page. Empty. How to add Application users

--- src/main/java/gifterland3/controllers/FacebookController.java
+++ src/main/java/gifterland3/controllers/FacebookController.java
@@ -4,15 +4,15 @@ package gifterland3.controllers;
  * Created by kdiv on 8/3/16.
  */
 import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.PagedList;
-import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
+//import javax.naming.Reference;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -41,6 +41,26 @@ public class FacebookController {
         PagedList<Post> feed = facebook.feedOperations().getFeed();
         model.addAttribute("feed", feed);
         return "facebookFeed";
+    }
+
+    @RequestMapping("/friends_list")
+    public String helloFacebookFriends(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+       // PagedList<Post> feed = facebook.feedOperations().getFeed();
+       // List<FacebookProfile> friends = facebook.friendOperations().getFriendProfiles();
+        //List<Reference> friends = facebook.friendOperations().getFriendLists();
+
+        //Facebook also enables users to organize their friends into friend lists. To retrieve a list of the authenticating user's friend lists, call getFriendLists() with no arguments:
+
+        PagedList<FriendList> friends = facebook.friendOperations().getFriendLists();
+
+       // model.addAttribute("feed", feed);
+        model.addAttribute("friends", friends);
+        return "friends_list";
     }
 
 }
